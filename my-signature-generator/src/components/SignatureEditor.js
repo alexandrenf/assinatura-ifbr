7e26a26b36982c5ce343a9f74cea16fe5865f4ad
alexandrenf <ale.borges8@me.com>
2024-10-31 00:13:01 +0000

    add

--- my-signature-generator/src/components/SignatureEditor.js
+++ my-signature-generator/src/components/SignatureEditor.js
@@ -7,11 +7,12 @@ function SignatureEditor() {
   const [year, setYear] = useState('2021/22');
   const [phone, setPhone] = useState('11 99999-9999');
 
-  const logoUrl = "https://i.ibb.co/DQwF246/logoemail2.png";
-  const fbIconUrl = "https://i.ibb.co/54sBsnL/icofb2.png";
-  const instaIconUrl = "https://i.ibb.co/JvjBb1m/icoinsta2.png";
-  const twitterIconUrl = "https://i.ibb.co/xq5DYpY/icotiktok2.png";
-
+  const logoUrl = "https://ifmsabrazil.org/images_24/logoemail2.png";
+  const fbIconUrl = "https://ifmsabrazil.org/images_24/icofb2.png";
+  const instaIconUrl = "https://ifmsabrazil.org/images_24/icoinsta2.png";
+  const twitterIconUrl = "https://ifmsabrazil.org/images_24/icotwitter.png";
+  const tiktokIconUrl = "https://ifmsabrazil.org/images_24/icotiktok4.png";
+  
   return (
     <div className="signature-editor">
       {/* Form Fields */}
@@ -75,7 +76,10 @@ function SignatureEditor() {
                         <img width="16" height="16" src={instaIconUrl} alt="Instagram" style={{ width: '16px', height: '16px', marginRight: '3px' }} />
                       </a>
                       <a target="_blank" href="https://tiktok.com/@ifmsabrazil" className="icone-inline">
-                        <img width="18" height="16" src={twitterIconUrl} alt="Twitter" style={{ width: '16px', height: '16px', marginRight: '5px' }} />
+                        <img width="16" height="16" src={tiktokIconUrl} alt="Twitter" style={{ width: '16px', height: '16px', marginRight: '3px' }} />
+                      </a>
+                      <a target="_blank" href="https://x.com/ifmsabrazil" className="icone-inline">
+                        <img width="18" height="16" src={twitterIconUrl} alt="Twitter" style={{ width: '16px', height: '16px', marginRight: '3px' }} />
                       </a>
                     </p>
                   </td>
